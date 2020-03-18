e41d583c17d50ad60e213a7d2597b032cfeebde7
astrahl008 <astra008@plattsburgh.edu>
2020-03-18 13:28:12 -0400

    Code passed all the tests. Could use optimization.

--- DataStructures/BasicDataStructures-11.js
+++ DataStructures/BasicDataStructures-11.js
@@ -11,19 +11,27 @@ function filteredArray(arr, elem) {
   var elem_count = 0;
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-      //console.log(j, arr[i].length);
-      if(arr[i][j] == elem) {
+      //console.log("j:",j, "len:",arr[i].length-1, "arr",arr[i][j], "elem:", elem, "elem_count",elem_count );
+      if(arr[i][j] === elem) {
         elem_count++;
       }
-      else if (j == arr[i].length-1 && elem_count == 0) {
+      else if (j === arr[i].length-1 && elem_count === 0) {
         newArr.push(arr[i]);
+        elem_count = 0;
+        console.log("elem:", elem, "pushed", arr[i]);
+      }
+      if (j === arr[i].length-1) {
         elem_count = 0;
       }
     }
   }
-  console.log(newArr);
+  console.log("returned:", newArr);
   // Only change code above this line
   return newArr;
 }
 
+/* Tests: */
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18) should return [ [10, 8, 3], [14, 6, 23] ]);
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2));  
+console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter") should return [ ["amy", "beth", "sam"] ]);
