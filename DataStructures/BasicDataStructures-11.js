611f27901b5ffaac0d532ae73f93fea7653bf1eb
astrahl008 <astra008@plattsburgh.edu>
2020-03-18 13:31:22 -0400

    Fixed Test cases

--- DataStructures/BasicDataStructures-11.js
+++ DataStructures/BasicDataStructures-11.js
@@ -31,7 +31,12 @@ function filteredArray(arr, elem) {
 }
 
 /* Tests: */
-console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18) should return [ [10, 8, 3], [14, 6, 23] ]);
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
+//should return [ [10, 8, 3], [14, 6, 23] ]
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
-console.log(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2));  
-console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter") should return [ ["amy", "beth", "sam"] ]);
+//should return [ ]
+console.log(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2));
+//should return [ ["flutes", 4] ] 
+console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter"));
+// should return [ ["amy", "beth", "sam"] ]
+console.log("The filteredArray function should utilize a for loop");
