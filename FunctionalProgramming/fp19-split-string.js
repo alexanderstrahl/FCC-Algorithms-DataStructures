396c3f8744cdb9a19540b05b6b6c2f50f62f1cfa
astrahl008 <astra008@plattsburgh.edu>
2020-04-05 17:15:12 -0400

    finished split string

---
+++ FunctionalProgramming/fp19-split-string.js
@@ -0,0 +1,9 @@
+function splitify(str) {
+    // Only change code below this line
+    
+    return str.split(/[-.,\s]/g);;
+  
+    // Only change code above this line
+  }
+  console.log(splitify("Hello World,I-am code"));
+  
