75e3e1f7a593ce6e58c5ca1473665c32db147681
astrahl008 <astra008@plattsburgh.edu>
2020-04-05 17:11:31 -0400

    sort array method

---
+++ FunctionalProgramming/sort-array-method.js
@@ -0,0 +1,12 @@
+/*
+Functional Programming: Sort an Array Alphabetically using the sort Method
+
+Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+*/
+function alphabeticalOrder(arr) {
+    // Only change code below this line
+    return arr.sort();
+    // Only change code above this line
+  }
+  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+  console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
