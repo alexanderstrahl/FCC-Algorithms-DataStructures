56deaff012abf8862602acf8673ee8415a097f03
astrahl008 <astra008@plattsburgh.edu>
2020-04-23 14:01:27 -0400

    Found the bug I was having. it was the arr.shift()

--- Algorithms/intermediate_Algorithms/ia16-steamroll.js
+++ Algorithms/intermediate_Algorithms/ia16-steamroll.js
@@ -3,18 +3,21 @@
 function steamrollArray(arr,flatten = []) {
     //let nArr = [...arr]
     arr.map(i => {
+      console.log(!Array.isArray(i),i)
       if(!Array.isArray(i)){
         flatten.push(i)
-        arr.shift()
+        //arr.shift()
+        console.log("push",i)
       }
       else if(Array.isArray(i)){
+        console.log("steamroll",i)
         steamrollArray(i, flatten)
       }
     })
-    console.log(flatten) 
+    console.log("flatten",flatten) 
     return flatten;
   }  
   
   steamrollArray([1, [2], [3, [[4]]]]);
-  steamrollArray([[["a"]], [["b"]]])
-  steamrollArray([1, {}, [3, [[4]]]])
+  //steamrollArray([[["a"]], [["b"]]])
+  //steamrollArray([1, {}, [3, [[4]]]])
