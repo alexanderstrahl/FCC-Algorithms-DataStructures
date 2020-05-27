6de46db616b1b2e510880f33326234d99957e5f5
astrahl008 <astra008@plattsburgh.edu>
2020-05-27 14:13:52 -0400

    working on project Number Validator

--- projects/4-number-validator.js
+++ projects/4-number-validator.js
@@ -1,7 +1,14 @@
 //JavaScript Algorithms and Data Structures Projects: Telephone Number Validator
 
 function telephoneCheck(str) {
-  return true;
+  let reg = (/[1(\s]*?\d{3}[)-\s]*?\d{3}[-]?\d{4}/g).test(str)
+  let reg2 = (/[\d]{10}/gi).test(str)
+
+  console.log(reg, reg2)
+  return reg;
 }
 
 telephoneCheck("555-555-5555");
+telephoneCheck("5555555555")
+telephoneCheck("1 (555) 555-5555")
+telephoneCheck("5555555") 
