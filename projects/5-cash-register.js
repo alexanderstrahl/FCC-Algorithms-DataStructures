65412ac0dd86a17e11af2acf17807cd4e785528a
astrahl008 <astra008@plattsburgh.edu>
2020-05-31 14:12:38 -0400

    Finished 4th test case one more to go

--- projects/5-cash-register.js
+++ projects/5-cash-register.js
@@ -8,9 +8,21 @@ function checkCashRegister(price, cash, cid) {
   //console.log("change",cash - price)
   let start_change = cash - price;
   //console.log(total_cash , cash-price)
-      if(total_cash < cash-price && total_cash == cash - price){
-        return {status: "INSUFFICIENT_FUNDS", change: []}
-      } 
+  if(total_cash < cash-price && total_cash == cash - price){
+    return {status: "INSUFFICIENT_FUNDS", change: []}
+  } 
+  let count = 0
+  for(let item = 0; item < cid.length; item++){
+    //console.log(item,cid.length-1)
+    if (item > 0){
+      count++
+      //return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+    if (total_cash < cash-price ){
+      return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
+  }
+  //cid.map(item => item > 0 ? false: {status: "INSUFFICIENT_FUNDS", change: []})
   while(cash > price){
     for(let i = cid.length-1; i >= 0; i--){
       //console.log(cash , price)
@@ -21,7 +33,7 @@ function checkCashRegister(price, cash, cid) {
       //console.log("i",i,(cash - price)/usd[i][1])
       
       if((cash - price)/usd[i][1] >= 1){ 
-        
+
           if((usd[i][1]*Math.trunc((cash - price)/usd[i][1])) > cid[i][1]){
             change.push([usd[i][0],cid[i][1]])
             change_closed[i][1] = (cid[i][1])
@@ -54,6 +66,6 @@ else{
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 
 //checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
-//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
 //console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
 //console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
